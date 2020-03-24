81fb984491a35fbbbdf09c234d9a92b3177def17
jacksonic777 <jacksonic777@gmail.com>
2020-03-24 12:05:55 +0900

    プロジェクト ファイルを追加します。

---
+++ KanjiDifferenceHunt/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace KanjiDifferenceHunt
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textHunt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textTimer = new System.Windows.Forms.TextBox();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textTimer);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.textHunt);
+            this.splitContainer1.Size = new System.Drawing.Size(684, 691);
+            this.splitContainer1.SplitterDistance = 70;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(89, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "を探せ";
+            // 
+            // textHunt
+            // 
+            this.textHunt.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textHunt.Location = new System.Drawing.Point(28, 12);
+            this.textHunt.Name = "textHunt";
+            this.textHunt.Size = new System.Drawing.Size(55, 39);
+            this.textHunt.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(185, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "記録：";
+            // 
+            // textTimer
+            // 
+            this.textTimer.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textTimer.Location = new System.Drawing.Point(268, 12);
+            this.textTimer.Name = "textTimer";
+            this.textTimer.Size = new System.Drawing.Size(141, 39);
+            this.textTimer.TabIndex = 3;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 691);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FormGame";
+            this.Text = "間違い探し";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textHunt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textTimer;
+    }
+}
+
---
+++ KanjiDifferenceHunt/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KanjiDifferenceHunt
+{
+    public partial class FormGame : Form
+    {
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
